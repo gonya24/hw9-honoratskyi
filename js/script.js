fca0f3bfc0368b59d99a3af888aed4e5b9f08182
Illia <illia.gonya@icloud.com>
2023-04-08 13:43:21 +0200

    2task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // 1 task
+// let result = '';
+// for (let i = 10; i<=20; i++) {
+//     result += i + (i == 20 ? '': ',')
+// }
+// console.log (result);
+//
+// 2 task
 let result = '';
 for (let i = 10; i<=20; i++) {
-    result += i + (i == 20 ? '': ',')
-}
+    result += i**2 + (i == 20 ? '': ',')
+ }
 console.log (result);
